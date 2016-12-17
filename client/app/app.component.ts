e03c5ecaf72dcd3885c74a521aa40303dbe07c93
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2016-12-16 22:09:50 -0500

    add text and textMax manipulation

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component  } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+const MAX_LENGTH = 50;
 
 @Component({
   selector: 'mon-app',
-  template: `
-    <h1>{{title}}</h1>
-    <p>{{value}}</p>
-  `
+  templateUrl: 'app/app.component.html',
+  styles: [`
+    .rouge { background-color: rgba(255,0,0,0.5); }
+    .vert  { background-color: rgba(0,128,0,0.5); }
+    textarea { transition: all .4s; }
+`]
 })
 
-export class AppComponent {
-  title = "Tour of heroes";
-  private value = "Hello";
+export class AppComponent implements OnInit {
+  texteATraduire: string;
+  longueurMax: number = MAX_LENGTH;
+  
+  ngOnInit(): void {
+    this.texteATraduire = "traduis moi";
+  }
 }
